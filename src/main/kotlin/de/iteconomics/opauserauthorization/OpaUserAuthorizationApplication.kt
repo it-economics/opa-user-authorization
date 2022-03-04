97d3b20d90ed8c4117196f7442a1a373a17a975a
Alex Kucksdorf <akucksdorf@it-economics.de>
2022-03-04 10:59:55 +0100

    chore: Add Spring Boot scaffold with web+security

---
+++ src/main/kotlin/de/iteconomics/opauserauthorization/OpaUserAuthorizationApplication.kt
@@ -0,0 +1,11 @@
+package de.iteconomics.opauserauthorization
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpaUserAuthorizationApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpaUserAuthorizationApplication>(*args)
+}
